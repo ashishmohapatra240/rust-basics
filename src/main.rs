4a4edf27e6a6ff4153d648d5d43844cb6e639dd5
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-28 17:28:09 +0530

    feat: implement area calculation for different shapes, using pattern matching and enums

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-enum Direction {
-    North,
-    South,
-    East,
-    West,
-    NorthEast,
-}
+use std::f32::consts::PI;
 
+enum Shape {
+    Circle(f32),
+    Rectangle(f32, f32),
+    Square(f32),
+}
 fn main() {
-    let direction = Direction::NorthEast;
-    steer(direction);
+    let shape: Shape = Shape::Square(10.0);
+    let shape_circle: Shape = Shape::Circle(10.0);
+    let shape_rectangle: Shape = Shape::Rectangle(10.0, 20.0);
+
+    println!("{}", area(shape));
+    println!("{}", area(shape_circle));
+    println!("{}", area(shape_rectangle));
 }
 
-fn steer(dir: Direction) {
-    match dir {
-        Direction::East => println!("Heading East"),
-        Direction::West => println!("Heading West"),
-        Direction::North => println!("Heading North"),
-        Direction::South => println!("Heading South"),
-        _ => println!("Heading somewhere"),
-    }
+fn area(shape: Shape) -> f32 {
+     return match shape {
+        Shape::Circle(radius) => radius * radius * PI,
+        Shape::Rectangle(width, height) => width * height,
+        Shape::Square(side) => side * side,
+    };
 }

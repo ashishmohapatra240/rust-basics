a7830e8c00ec82e23982c7bad1f880773a043d86
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-28 18:18:55 +0530

    feat: implement find_first function =, learned Option

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::fs;
-
 fn main() {
-    let contents = fs::read_to_string("a.txt");
-
-    match contents {
-        Ok(contents) => println!("{}", contents),
-        Err(e) => println!("Error: {}", e),
+    let ans = find_first(String::from("Ashish"));
+    match ans {
+        None => println!("None"),
+        Some(value) => println!("{}", value),
     }
+}
+
+fn find_first(str: String) -> Option<u32> {
+    let mut index = 0;
+    for c in str.chars() {
+        index += 1;
+        if c == 'a' {
+            return Some(index);
+        }
+    }
+    None
 }

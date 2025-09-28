e68c16dbe1935868d8d3ff897250a77697ae5091
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-28 15:37:11 +0530

    refactor: implement string borrowing, mutability and immutability

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
 fn main() {
-    let ans = sum(1, 2);
-    println!("{}", ans);
-    let is_even = is_even(1);
-    println!("{}", is_even);
+    let mut s1: String = String::from("Ashish");
+    let s2 = &mut s1;
+    let s3 = &s1;
+    let s4 = &s1;
 
-    let name = String::from("Ashish");
-    println!("{}", name);
+    println!("{}", s1);
+    println!("{}", s2);
+    println!("{}", s3);
+    println!("{}", s4);
 
-    let v: Vec<i32> = vec![1, 2, 3, 4, 5];
-    println!("{:?}", v);
 
-    for i in 0..100 {
-        print!("{} ", i);
-    }
 
-    let mut a = 1;
-    a = 2;
-    println!("{}", a);
-
-    let mut name: String = String::from("Ashish");
-
-    let name2 = name.clone();          //dangling pointer error
-    name.push_str(" Mohapatra");
-    println!("{}", name2);
-
-    let name3 = String::from("Ashish");
-    let (len, name3) = get_len(name3);
-    println!("{}", name3);
-    println!("{}", len);
-}
-
-fn sum(a: u32, b: u32) -> u32 {
-    return a + b;
-}
-
-fn is_even(a: u32) -> bool {
-    return a % 2 == 0;
-}
-
-fn get_len(s: String) -> (usize, String) {
-    return (s.len(), s);
 }

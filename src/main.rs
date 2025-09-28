b9967fd3beccc55b625e93c5aa4fff4b8a71890c
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-28 18:04:15 +0530

    feat: add a.txt file and update main function to read and print file contents

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
-use std::f32::consts::PI;
-
-enum Shape {
-    Circle(f32),
-    Rectangle(f32, f32),
-    Square(f32),
-}
-
-impl Shape {
-    fn area(&self) -> f32 {
-        return match self {
-            Shape::Circle(radius) => radius * radius * PI,
-            Shape::Rectangle(width, height) => width * height,
-            Shape::Square(side) => side * side,
-        };
-    }
-}
+use std::fs;
 
 fn main() {
-    let shape: Shape = Shape::Square(10.0);
-    let shape_circle: Shape = Shape::Circle(10.0);
-    let shape_rectangle: Shape = Shape::Rectangle(10.0, 20.0);
+    let contents = fs::read_to_string("a.txt");
 
-    println!("{}", shape.area());
-    println!("{}", shape_circle.area());
-    println!("{}", shape_rectangle.area());
+    match contents {
+        Ok(contents) => println!("{}", contents),
+        Err(e) => println!("Error: {}", e),
+    }
 }

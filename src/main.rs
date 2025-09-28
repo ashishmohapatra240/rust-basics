133ba4feedcc2788b4e76c7cb7fdc0bbf8eea500
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-28 17:33:01 +0530

    refactor: move area calculation into Shape implementation and update main function to use method calls

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,23 @@ enum Shape {
     Rectangle(f32, f32),
     Square(f32),
 }
+
+impl Shape {
+    fn area(&self) -> f32 {
+        return match self {
+            Shape::Circle(radius) => radius * radius * PI,
+            Shape::Rectangle(width, height) => width * height,
+            Shape::Square(side) => side * side,
+        };
+    }
+}
+
 fn main() {
     let shape: Shape = Shape::Square(10.0);
     let shape_circle: Shape = Shape::Circle(10.0);
     let shape_rectangle: Shape = Shape::Rectangle(10.0, 20.0);
 
-    println!("{}", area(shape));
-    println!("{}", area(shape_circle));
-    println!("{}", area(shape_rectangle));
-}
-
-fn area(shape: Shape) -> f32 {
-     return match shape {
-        Shape::Circle(radius) => radius * radius * PI,
-        Shape::Rectangle(width, height) => width * height,
-        Shape::Square(side) => side * side,
-    };
+    println!("{}", shape.area());
+    println!("{}", shape_circle.area());
+    println!("{}", shape_rectangle.area());
 }

80aad2962e49d9f4d070ce395c3232f83a08ae5a
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-30 01:10:04 +0530

    feat: implemented a macro

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,8 @@
-struct Rect{
-    width: f32,
-    height: f32,
+macro_rules! say_hello {
+    () => {
+        println!("Hello, world!");
+    };
 }
-
-impl Shape for Rect{
-    fn area(&self) ->f32{
-        return self.width * self.height;
-    }
-
-}
-struct Circle{
-    radius: f32,
-}
-
-
-impl Shape for Circle{
-    fn area(&self) ->f32{
-        return self.radius * self.radius * 3.14;
-    }
-}
-
-trait Shape{
-    fn area(&self) ->f32;
-}
-
-
-fn print_area_of_shape<T: Shape>(shape: T){
-    println!("{}", shape.area());
-}
-
 fn main() {
-    let r = Rect{width: 10.0, height: 20.0};
-    let c = Circle{radius: 10.0};
-    print_area_of_shape(r);
-    print_area_of_shape(c);
-
+    say_hello!();
 }

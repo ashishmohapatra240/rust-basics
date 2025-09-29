0b7174e2e1870ef61fcef2107703414e3ee81e44
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-29 16:01:24 +0530

    refactor: implement sum function in main using generics

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 fn main() {
-    let ans = find_first(String::from("Ashish"));
-    match ans {
-        None => println!("None"),
-        Some(value) => println!("{}", value),
-    }
+    println!("{}", sum(1, 2));
 }
 
-fn find_first(str: String) -> Option<u32> {
-    let mut index = 0;
-    for c in str.chars() {
-        index += 1;
-        if c == 'a' {
-            return Some(index);
-        }
-    }
-    None
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T{
+    return a + b;
 }

290ce9433aacad3647da300a225f59ee451557e3
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-29 16:24:02 +0530

    feat: sum with different types and handle type, generics and traits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    println!("{}", sum(1, 2));
+    let s1 = sum(1.1, 2.2);
+    let s2 = sum(1, 2);
+    let s3 = sum(true, false);    //cannot add bool to bool, because bool does not implement the Add trait
+    println!("{}", s2);
+    println!("{}", s1);
 }
 
-fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T{
+fn sum<T: std::ops::Add<Output =T>>(a:T, b:T) -> T{
     return a + b;
 }
